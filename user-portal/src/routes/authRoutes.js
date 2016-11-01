f91ba7b847f1f0c4988dd76e3c7c9f111c0c6245
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2016-11-01 14:40:48 -0400

    testing direct redirect to itrust mapping

--- user-portal/src/routes/authRoutes.js
+++ user-portal/src/routes/authRoutes.js
@@ -153,9 +153,10 @@ var router = function (logger, config, db, mailer) {
                         req.session.email = document.mail;
                         req.session.username = document.extracted_dn_username;
                         db.log(userObject, 'UUID confirmed. Proceeding with user mapping.');
-                        res.render('jump', {
-                            uuid: id
-                        });
+                        res.redirect('/protected/itrust/map/' + id);
+                        // res.render('jump', {
+                        //     uuid: id
+                        // });
                     }
                 } else {
                     logger.warn('UUID ' + id + ' sent for confirmation. No document found!');
