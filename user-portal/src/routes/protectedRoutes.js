6da9093f9517b7b25b9c03cc425d06c749d1baef
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2016-11-28 15:19:39 -0500

    access-request for dev

--- user-portal/src/routes/protectedRoutes.js
+++ user-portal/src/routes/protectedRoutes.js
@@ -121,19 +121,19 @@ var router = function (logger, config, db, mailer) {
     protectedRouter.route('/access-request')
         .post(function (req, res) {
             var app = req.body.app.toLowerCase().trim();
-            // var userDN = req.get('smuserdn').toLowerCase();
+            var userDN = req.get('smuserdn').toLowerCase();
             var accessLevel = req.body.acclevel;
             var justification = req.body.justification;
 
             var subject = 'NCI Application Access Request';
             var message = '<p>Access was requested for application: ' + app + '</p>' +
-                // '<p>User DN: the user DN' + userDN + '</p>' + 
-                '<p>User DN: the user DN' + '</p>' +
+                '<p>User DN: the user DN' + userDN + '</p>' + 
+                // '<p>User DN: the user DN' + '</p>' +
                 '<p>Access Level: ' + accessLevel + '</p>' +
                 '<p>Justification: ' + justification + '</p>';
 
             mailer.send('svetoslav.yankov@nih.gov', subject, message);
-            // logger.info('Access request submitted for application: ' + app + ', Eser DN: ' + userDN + ', access level requested: ' + accessLevel)
+            logger.info('Access request submitted for application: ' + app + ', Eser DN: ' + userDN + ', access level requested: ' + accessLevel)
 
             res.redirect('/logoff');
 
