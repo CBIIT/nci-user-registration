82c9a5212587d122bae6ee7ead48d7a7f8c1a659
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2016-12-12 21:40:22 -0500

    adds prefix to registration report admin email

--- user-portal/src/routes/protectedRoutes.js
+++ user-portal/src/routes/protectedRoutes.js
@@ -44,7 +44,7 @@ var router = function (logger, config, db, mailer) {
         } else if (sm_userdn === '') {
             logger.warn('User account was provisioned, but sm_userdn is empty. User will be advised to attempt mapping in 24 hours.');
             db.log(userObject, 'User attempted registration with empty sm_userdn. Headers: ' + JSON.stringify(req.headers));
-            mailer.send(config.mail.admin_list, 'Empty sm_userdn registration attempt', 'Headers: ' + JSON.stringify(req.headers));
+            mailer.send(config.mail.admin_list, config.mail.subjectPrefix + ' ### Empty sm_userdn registration attempt', 'Headers: ' + JSON.stringify(req.headers));
             res.redirect('/logoff?pending=true');
         } else {
 
@@ -89,7 +89,7 @@ var router = function (logger, config, db, mailer) {
                                 res.redirect('/logoff?mapped=true');
                             } else {
                                 db.log(userObject, 'Mapped to sm_userdn ' + sm_userdn + ', which is and invalid DN. Record was flagged for manual processing. Headers: ' + JSON.stringify(req.headers));
-                                mailer.send(config.mail.admin_list, 'Registration with invalid sm_userdn', 'Headers: ' + JSON.stringify(req.headers));
+                                mailer.send(config.mail.admin_list, config.mail.subjectPrefix +' ### Registration with invalid sm_userdn', 'Headers: ' + JSON.stringify(req.headers));
                                 res.redirect('/logoff?invaliddn=true');
                             }
                         }
